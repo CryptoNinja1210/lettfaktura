eb730629146b6dfaf76b4cf370c5a6b578e43f09
CryptoNinja1210 <crypto.ninja1210@gmail.com>
2023-12-20 00:36:35 +0900

    allowhost4

--- lettfaktura/urls.py
+++ lettfaktura/urls.py
@@ -23,5 +23,5 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('/', include('product.urls')),
 ]
-if settings.DEBUG:
-    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+# if settings.DEBUG:
+#     urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)

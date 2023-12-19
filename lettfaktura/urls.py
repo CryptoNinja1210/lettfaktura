4359f5ffdf2d16c86c424ef9efc2d474d3cc4648
CryptoNinja1210 <crypto.ninja1210@gmail.com>
2023-12-20 00:39:38 +0900

    url

--- lettfaktura/urls.py
+++ lettfaktura/urls.py
@@ -21,7 +21,7 @@ from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('/', include('product.urls')),
+    path('', include('product.urls')),
 ]
 # if settings.DEBUG:
 #     urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)

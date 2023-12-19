067d652ddffd80872b15297e7ee61de22c8075d1
CryptoNinja1210 <crypto.ninja1210@gmail.com>
2023-12-19 22:05:01 +0900

    cc

--- lettfaktura/urls.py
+++ lettfaktura/urls.py
@@ -14,12 +14,14 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+import statistics
+from django.conf import settings
 from django.contrib import admin
 from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('product/', include('product.urls')),
+    path('/', include('product.urls')),
 ]
 if settings.DEBUG:
-    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+    urlpatterns += statistics(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
--- product/urls.py
+++ product/urls.py
@@ -2,8 +2,8 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.insert_pro, name='insert-pro'),
-    path('show/', views.show_pro, name='show-pro'),
+    path('save/', views.insert_pro, name='insert-pro'),
+    path('', views.show_pro, name='show-pro'),
     path('edit/<int:pk>', views.edit_pro, name='edit-pro'),
     path('remove/<int:pk>', views.remove_pro, name='remove-pro'),
 ]

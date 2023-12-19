89c1382bb02f677d261fbf94761df25449bc9882
CryptoNinja1210 <crypto.ninja1210@gmail.com>
2023-12-19 23:56:52 +0900

    db-test

--- build_files.sh
+++ build_files.sh
@@ -1,6 +1,6 @@
 python3 -m venv env
 source env/bin/activate
 pip install -r requirements.txt
-python3 manage.py migrate
-python3 manage.py collectstatic
+# python3 manage.py migrate
+# python3 manage.py collectstatic
 # python3 manage.py collectstatic --noinput
